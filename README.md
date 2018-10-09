cf3e330c432883faf875ebf8db28ec30b6d4d661
Gordon Woodhull <gordon@woodhull.com>
2018-10-09 09:16:32 -0400

    working with dc.datatables 0.0.6

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# AngDctable
+# ang-dctable
+
+This project tests [dc.datatables](https://github.com/dc-js/dc.datatables.js) in Angular 6.
+
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.2.4.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as dc from 'dc';
+import * as dctable from 'dc.datatables';
 
 @Component({
   selector: 'app-root',
@@ -8,3 +10,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ang-dctable';
 }
+console.log(dc);
+console.log(dctable)
